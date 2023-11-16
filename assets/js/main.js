79b54a986703026641b556cc87bf0b16bee6525c
KhuatCong <khuatphucong06092002@gmail.com>
2023-11-16 11:46:57 +0700

    update

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function openSearch(){
+    document.getElementById('search-model').style.height="auto";
+    document.getElementById('search-model').style.padding="30px 100px";
+}
+
+function closeSearch(){
+    document.getElementById('search-model').style.height="0";
+    document.getElementById('search-model').style.padding="0";
+}
